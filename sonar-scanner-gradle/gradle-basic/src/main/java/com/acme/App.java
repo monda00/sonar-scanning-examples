c11eb902ac6a0ae48ffc1969050a52cb964f12be
monda00 <monda0524@gmail.com>
2024-08-29 20:42:36 +0900

    test commit

--- sonar-scanner-gradle/gradle-basic/src/main/java/com/acme/App.java
+++ sonar-scanner-gradle/gradle-basic/src/main/java/com/acme/App.java
@@ -5,7 +5,12 @@ public class App {
         return "Hello world.";
     }
 
+    public String getGreetingWithName(String name) {
+        return "Hello " + name + ".";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreetingWithName("Sam"));
     }
 }
--- sonar-scanner-gradle/gradle-basic/src/test/java/com/acme/AppTest.java
+++ sonar-scanner-gradle/gradle-basic/src/test/java/com/acme/AppTest.java
@@ -3,10 +3,17 @@ package com.acme;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+
+    @Test
+    public void testAppHasAGreetingWithName() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreetingWithName("Sam"), "app should have a greeting");
     }
 }
